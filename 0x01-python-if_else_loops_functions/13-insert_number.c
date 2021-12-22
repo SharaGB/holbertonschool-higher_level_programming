a539acc09c17c06ba89db2938d71bc096075f166
SharaGB <shara.garcia.betancur@gmail.com>
2021-12-21 23:50:51 -0500

    Function that inserts a number singly linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief 
+ * 
+ */
+ listint_t *insert_node(listint_t **head, int number)
+ {
+     int key = number;
+     listint_t *tmp = *head;
+     listint_t *new = malloc(sizeof(listint_t));
+
+     new->n = number;
+     new->next = NULL;
+     if (head == NULL || key < (*head)->n)
+     {
+         new->next = *head;
+         *head = new;
+     }
+     else
+     {
+         tmp = *head;
+         {
+             while (tmp->next != NULL && tmp->next->n < key)
+             {
+                 tmp = tmp->next;
+             }
+             new->next = tmp->next;
+             tmp->next = new;
+         }
+     }
+     return (*head);
+ }
