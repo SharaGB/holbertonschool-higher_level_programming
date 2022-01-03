32af8aa3137f5930d001964346db4068585cada7
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-03 18:34:23 -0500

    Fix second conditional for

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for mtrx in matrix:
-        print(' '.join("{}".format(x) for x in mtrx))
+        print(' '.join("{:d}".format(x) for x in mtrx))
