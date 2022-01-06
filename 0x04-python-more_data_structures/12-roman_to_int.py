05371bb76f918b6a60b430ff13fbe8d0a194d0bb
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-06 01:59:23 -0500

    Fix pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
         if roman.get(roman_string[i], 0) == 0:
             return 0
         if i != len(roman_string) - 1\
-            and roman[roman_string[i]] < roman[roman_string[i + 1]]:
+                and roman[roman_string[i]] < roman[roman_string[i + 1]]:
             num -= roman[roman_string[i]]
         else:
             num += roman[roman_string[i]]
