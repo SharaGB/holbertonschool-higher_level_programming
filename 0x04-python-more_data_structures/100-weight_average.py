60e3727849b399879d7c30cc572411ce4dad7b8d
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-05 22:07:55 -0500

    Fix return list empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
     normalization = 0
     val = 0
+    if len(my_list) == 0:
+        return 0
     for value, weight in my_list:
         val += value * weight
         normalization += weight
