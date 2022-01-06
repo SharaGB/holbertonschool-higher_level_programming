db626c0e2de0cdd83ec42f0f0447665e2bef8736
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-05 23:47:13 -0500

    Fix pycodestyle

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    biggest = max(a_dictionary.items(), key = lambda x: x[1]) [0]
+    biggest = max(a_dictionary.items(), key=lambda x: x[1])[0]
     return biggest
