f77f9b2efcd59b168e95a726575c82db3279c52d
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-06 01:16:34 -0500

    Fix pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not type(roman_string) == str and roman_string is None:
+    if type(roman_string) is not str and roman_string is None:
         return 0
 
     s = roman_string.upper()
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-             'C': 100, 'D': 500, 'M': 1000}
+    roman = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50,
+             'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
     num = 0
 
     for i in range(len(roman_string)):
