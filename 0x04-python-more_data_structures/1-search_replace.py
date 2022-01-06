415eb89cb88a74e17d51a41f97a6b751ab626791
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-05 21:10:09 -0500

    Fix range

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for elm in range(len(my_list)):
+    for elm in range(0, len(my_list)):
         if my_list[elm] == search:
             my_list[elm] = replace
     return new_list
