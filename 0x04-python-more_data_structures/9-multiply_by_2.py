c4b2ea770d2f012ef1e2700844e381ac8c7138e4
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-05 23:27:25 -0500

    Fix the return

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: value * 2 for key, value in sorted(a_dictionary.items())}
+    new_dict = {key: value * 2 for key, value in sorted(a_dictionary.items())}
+    return new_dict
