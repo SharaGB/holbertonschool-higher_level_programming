2cc593d2c94def7872a3ea57000af4191fc98254
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-05 22:17:08 -0500

    Fix file because had more 3 lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_mtrx = [list(map(lambda x: x ** 2, lists)) for lists in matrix]
-    return new_mtrx
+    return [list(map(lambda x: x ** 2, lists)) for lists in matrix]
