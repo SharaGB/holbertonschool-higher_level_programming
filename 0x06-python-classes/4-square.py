3d2a606cd2deced43eac4aa5f187983448a1e523
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-18 18:32:48 -0500

    Define a Square with property and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Empty class Square definition"""
+
+
+class Square:
+    """A class Square that defines a square by:
+        - Private instance attribute: size"""
+
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size: int = size
+
+    @property
+    def size(self):
+        """Property def size(self): to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Property setter def size(self, size): to set it"""
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public instance method"""
+        return self.__size ** 2
