74774e906f1c4192e06aba5994829a3a6f7380fb
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-17 21:22:33 -0500

    Change returns eith one

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     element = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end='')
             element += 1
-        print()
-        return element
-    except IndexError:
-        print()
-        return element
+        except IndexError:
+            pass
+    print()
+    return element
