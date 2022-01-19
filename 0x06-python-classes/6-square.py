bdf7f5209dcda02487b0aa9dca0215c7b34b33d6
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-19 16:23:00 -0500

    Fix print square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,9 +61,9 @@ class Square:
             stdout the square with the character #"""
         if self.__size == 0:
             print()
-        if self.__position[1] > 0:
-            for space in range(self.__position[1]):
-                print()
+        # if self.__position[1] > 0:
+        #     for space in range(self.__position[1]):
+        #         print()
         for i in range(self.__size):
             print(" " * self.__position[0], end='')
             print("#" * self.__size)
