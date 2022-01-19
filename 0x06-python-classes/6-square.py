7d0a11e97401d96d72748cd15195dd62d36b3889
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-18 21:38:25 -0500

    Fix print instance

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,14 +44,10 @@ class Square:
             - position must be a tuple of 2 positive integers, otherwise
             raise a TypeError exception with the message position must be
             a tuple of 2 positive integers"""
-        if type(value) is tuple or len(value) == 2:
-            self.__position = value
-        elif type(value[0]) is int or type(value[1]) is int:
-            self.__position = value
-        elif value[0] > 0 or value[1] > 0:
-            self.__position = value
-        else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+        for idx in value:
+            if type(idx) is not int or len(value) != 2 or idx < 0:
+                raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """Public instance method:
