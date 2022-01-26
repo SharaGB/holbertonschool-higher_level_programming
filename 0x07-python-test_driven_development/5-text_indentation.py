5431989edec0f42d5192db225e40c3fdab39522f
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-25 20:35:10 -0500

    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is the ``text_indentation`` module.
+
+The text_indentation module supplies one function, text_indentation(text)
+"""
+
+
+from hashlib import new
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    delimiter = 0
+    while delimiter < len(text):
+        if text[delimiter] == "." or text[delimiter] == "?"\
+                or text[delimiter] == ":":
+            text = text[:(delimiter + 1)] + "\n\n" + text[(delimiter + 1):]
+        delimiter += 1
+    new_text = "\n".join(string.strip() for string in text.splitlines(1))
+    print(new_text, end='')
