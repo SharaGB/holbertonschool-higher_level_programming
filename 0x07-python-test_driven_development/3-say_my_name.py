091a6ab811772073661c093cc075397311476d38
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-25 20:03:00 -0500

    Write a function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is the ``say_my_name`` module.
+
+This supplies one function, say_my_name(first_name, last_name="")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that prints My name is <first name> <last name>:
+        >>> say_my_name("Jon", "Snow")
+        My name is Jon Snow"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
