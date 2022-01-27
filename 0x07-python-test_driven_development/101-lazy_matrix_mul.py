8a43fea8f3421c3680e6b5b6575d8c485fe1cf34
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-26 21:40:35 -0500

    Fix code fail

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,11 +4,11 @@ This is the ``lazy_matrix_mul`` module.
 
 The lazy_matrix_mul module supplies one function, add_integer(a, b=98)
 """
-import numpy as np
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
     Function that multiplies 2 matrices by using the module NumPy
     """
-    return np.dot(m_a, m_b)
+    return numpy.dot(m_a, m_b)
