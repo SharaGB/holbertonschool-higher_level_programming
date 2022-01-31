095f772faf6221bd43099a3b3fc7011845eff38c
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-31 18:36:13 -0500

    Class based on task 5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the ``base_geometry`` module.
+"""
+
+
+class BaseGeometry:
+    """ Public instance method: def area(self). """
+    pass
+
+    def area(self):
+        """ Raises an Exception. """
+        raise Exception("area() is not implemented")
