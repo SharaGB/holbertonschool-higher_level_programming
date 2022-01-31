b3bb2495dd0592cb43ccc34bef00fe4a97da414a
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-31 18:40:17 -0500

    A class that inherits from task 7

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the ``rectangle`` module.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle that inherits from BaseGeometry. """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator('width', self.__width)
+        self.integer_validator('height', self.__height)
