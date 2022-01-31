100fe033d1c311dee870b17be1dce11e0f507183
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-31 18:33:23 -0500

    Add function with isintance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the ``is_kind_of_class`` module.
+
+This module supplies one function, is_kind_of_class(obj, a_class)
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ The isinstance() function returns True if the specified object
+            is of the specified type, otherwise False"""
+    return isinstance(obj, a_class)
