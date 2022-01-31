1a59d39a1de2cecf67b5b8ed1cda512272310dc5
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-31 18:37:00 -0500

    Write class based on task 6

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is the ``base_geometry`` module.
+"""
+
+
+class BaseGeometry:
+    """ - Public instance method: def area(self).
+        - Public instance method: def integer_validator(self, name, value)"""
+    pass
+
+    def area(self):
+        """ Raises an Exception. """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates value."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
