361c6f8cf842c63d936f9f2292135dcde88eb318
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-31 21:39:34 -0500

    Add class MyInt

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the ``100-my_int`` module.
+"""
+
+
+class MyInt(int):
+    """ Class MyInt that inherits from int. """
+    def __eq__(self, x) -> bool:
+        return False
+
+    def __ne__(self, x) -> bool:
+        return True
