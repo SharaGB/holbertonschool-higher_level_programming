8dae3986fbe482db4fd62e45bb1811cb2c564918
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-31 21:45:51 -0500

    Return true and false

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,7 +8,7 @@ class MyInt(int):
     """ Class MyInt that inherits from int. """
 
     def __eq__(self, x) -> bool:
-        return int(self) != x
+        return int(self) != x  # Retorna False
 
     def __ne__(self, x) -> bool:
-        return int(self) == x
+        return int(self) == x  # Retorna True
