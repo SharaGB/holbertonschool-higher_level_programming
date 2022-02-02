d3353e509c9d8d48ccdd7e6c814b0138aec1c285
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-01 23:33:06 -0500

    Fix code fail

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,12 +4,19 @@ This is the ``100-append_after`` module.
 """
 
 
+from hashlib import new
+
+
 def append_after(filename="", search_string="", new_string=""):
     """ Inserts a line of text to a file, after each line
             containing a specific string"""
     with open(filename, mode='r') as file:
         lines = file.readlines()
-    idx = 0
-    lines.insert(idx, new_string)
+    data = ""
+    for line in lines:
+        data += line
+        if search_string in line:
+            data += new_string
+
     with open(filename, mode='w') as file:
-        file.writelines(lines)
+        file.writelines(data)
