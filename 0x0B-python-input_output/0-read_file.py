562f86aeefd4835e37a464dc175e82e7849f5269
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-01 19:07:51 -0500

    Function that reads a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the ``0-read_file`` module.
+"""
+
+
+def read_file(filename=""):
+    """ Function that reads a text file (UTF8). """
+    with open("my_file_0.txt", mode='r', encoding='utf-8') as a_file:
+        print(a_file.read(), end='')
