21a8d2fb9c6e3f6a60e2f231bd9fd81b1249c4f2
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-01 21:24:58 -0500

    Add task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This is the ``11-student`` module.
+"""
+
+
+class Student:
+    """ Class Student that defines a student by:
+        - Public instance attributes:
+            first_name
+            last_name
+            age.
+        - Public method def to_json(self, attrs=None)
+        - Public method def reload_from_json(self, json) """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes the data. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student. """
+        attribute_names = {}
+        if isinstance(attrs, list):
+            for x in attrs:
+                if x in self.__dict__:
+                    attribute_names[x] = self.__dict__[x]
+            return attribute_names
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student. """
+        for key in json:
+            self.__dict__[key] = json[key]
