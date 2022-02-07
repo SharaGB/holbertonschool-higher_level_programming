6a1b2d7d592a21c922fa195e5497be2d915194ee
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-07 15:11:02 -0500

    Add test cases

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest for base([..])
+"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """
+    Write unittests for the class Base
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
