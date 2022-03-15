103826addf442341e5ee41cab08b73c9c7d79801
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-14 23:16:42 -0500

    Updates the score of Bob to 10 in the table

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- Updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field
+UPDATE LOW_PRIORITY IGNORE second_table
+SET
+    score = 10
+WHERE
+    name = 'Bob';
