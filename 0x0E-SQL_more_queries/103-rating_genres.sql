18c2bebf936e35ceffc3fccad4633c9b3c16db01
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-17 13:12:46 -0500

    Fix join tv_genres

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,6 +3,6 @@
 -- Results must be sorted in descending order by their rating
 
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings
-JOIN tv_show_genres ON  tv_show_ratings.show_id = tv_show_genres.genre_id
-JOIN tv_genres ON tv_genres.id = tv_show_ratings.show_id
+JOIN tv_show_genres ON tv_show_ratings.show_id = tv_show_genres.genre_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.show_id
 GROUP BY tv_shows.name ORDER BY rating DESC;
