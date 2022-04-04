be47b1d9e0d60e4151132510670d127cdc55f504
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-04 16:06:44 -0500

    Write a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function prints(a, b, c) {
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    console.log(arguments[2]);
+  }
+
+prints('C is fun', 'Python is cool', 'JavaScript is amazing');
