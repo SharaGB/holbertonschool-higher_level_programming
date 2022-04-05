104ec3f16d2e5a796f9675b041dca9cc22eb64fa
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-04 20:13:43 -0500

    Fix the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,8 @@ const argc = process.argv.slice(2);
 if (argc.length < 2) {
   console.log(0);
 } else {
-  argc.sort();
+  argc.sort(function(a, b){
+      return a - b
+  });
   console.log(argc[argc.length - 2]);
 }
