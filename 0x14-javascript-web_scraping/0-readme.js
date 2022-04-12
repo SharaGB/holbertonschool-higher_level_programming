7dfff4507cc509776198a5db4bda47bea2a0b5b7
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-11 23:42:47 -0500

    Script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, (err, data) => {
+  if (err) {
+    return console.error((err));
+  }
+  console.log(data.toString('utf8'));
+});
