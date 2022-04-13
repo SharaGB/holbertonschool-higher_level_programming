1b23d3a079667384fddbbabf56074330c750969b
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-13 16:32:45 -0500

    Adds, removes and clears LI elements from a list when the user clicks

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  $('#add_item').click(() => {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+
+  $('#remove_item').click(() => {
+    $('UL.my_list>li:last-child').remove();
+  });
+
+  $('#clear_list').click(() => {
+    $('UL.my_list').empty();
+  });
+};
