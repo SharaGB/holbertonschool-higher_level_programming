6abf29e041517bbecd4bc411cc2483627ad8348b
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-09 20:10:36 -0500

    Write Script one that is safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in an argument and displays all values in the states table
-    of hbtn_0e_0_usa where name matches the argument.
+Write Script one that is safe from MySQL injections
 """
 import MySQLdb
 from sys import argv
