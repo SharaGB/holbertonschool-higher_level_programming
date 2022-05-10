32f4710e0e7fd34c122d34ea4485d9445ac8ed23
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-09 19:29:17 -0500

    Displays all values in the states table of a argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Takes in an argument and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    # Default host is "localhost"
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    try:
+        # Get data from database
+        cursor.execute("SELECT * FROM states WHERE name = '{}' \
+                        ORDER BY states.id ASC".format(argv[4]))
+        rows = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("MYSQL Error: %s", str(e))
+
+    # Print results
+    for row in rows:
+        print(row)
+    # Close all cursors and all databases
+    cursor.close()
+    db.close()
