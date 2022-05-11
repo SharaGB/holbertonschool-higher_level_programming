6e5461a503db2e5d927e52615e308a68470484ea
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-10 22:14:49 -0500

    Creates the State “California” with the City “San Francisco”

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Creates the State “California” with the City “San Francisco”
+"""
+import sys
+from model_city import City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Create an engine
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
+        @localhost/{sys.argv[3]}', pool_pre_ping=True)
+    # Create a metadata instance (Table)
+    Base.metadata.create_all(engine)
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    session = Session()  # Create a new session
+    newState = State(name='California')
+    newState.city = City(name='San Francisco')
+    session.add(newState)
+    # Issue all remaining changes to the database and commit the transaction
+    session.commit()
+    session.close()  # Close session
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class definition of a State and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+Base = declarative_base()
+
+
+class State(Base):
+    """ State class that inherits from Base """
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    city = relationship("City", back_populates="state")  # Or backref
