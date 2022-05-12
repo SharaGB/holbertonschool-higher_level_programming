ddfe36404d46373b6056be2988afc35b20049acf
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-11 20:31:13 -0500

    Fix relationship between state and cities

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,10 +3,10 @@
 Creates the State “California” with the City “San Francisco”
 """
 import sys
-from model_city import City
-from model_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
 
 if __name__ == "__main__":
     # Create an engine
@@ -17,8 +17,9 @@ if __name__ == "__main__":
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
     session = Session()  # Create a new session
+
     newState = State(name='California')
-    newState.cities = City(name='San Francisco')
+    newState.cities = [City(name='San Francisco')]
     session.add(newState)
     # Issue all remaining changes to the database and commit the transaction
     session.commit()
