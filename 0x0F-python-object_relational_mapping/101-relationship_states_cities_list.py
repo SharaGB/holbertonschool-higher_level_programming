09ddcd12e3a74a4e04619d246f66a2249a532854
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-11 21:01:37 -0500

    Fix lists all State objects, and corresponding City objects

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,6 +6,7 @@ Lists all State objects, and corresponding City objects,
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import City
 from relationship_state import Base, State
 
 if __name__ == "__main__":
