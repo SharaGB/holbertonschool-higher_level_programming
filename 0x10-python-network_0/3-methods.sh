fb8543052a273569f75bd2ae3d032bd4488dbbbe
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-18 12:56:04 -0500

    Fix displays all HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS "$1" | grep 'Allow:' | sed -n 's/Allow: //p'
+curl -sIX OPTIONS "$1" | sed -n 's/Allow: //p'
