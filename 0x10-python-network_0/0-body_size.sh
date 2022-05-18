259e2e3de0fb1e1b6b871dfb9f03a91d080832b6
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-18 12:06:07 -0500

    Fix requirement that should be exactly 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -i Content-Length | cut -d' ' -f2 # awk '{print $2}' works too!, but it's slower than cut
+curl -sI "$1" | awk '/Content-Length/ { print $2 }'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Takes in a URL, sends a GET request to the URL, and displays the body of the response
-URL="$1"
-curl -L $URL
+# Takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
