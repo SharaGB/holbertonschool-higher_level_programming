92ab58cc69e414a29cf3329fb95b928d548f0045
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-18 12:04:09 -0500

    Fix sends a request an to URL

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# URL="$1"
-curl -sI $1 | grep -i Content-Length | cut -d' ' -f2 # awk '{print $2}' works too!, but it's slower than cut
+curl -sI "$1" | grep -i Content-Length | cut -d' ' -f2 # awk '{print $2}' works too!, but it's slower than cut
