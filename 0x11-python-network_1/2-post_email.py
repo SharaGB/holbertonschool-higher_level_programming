4af51f76d7beb920daedc04915948b5ed149ed83
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 18:05:07 -0500

    Sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8) """
+        and displays the body of the response """
 import urllib.parse
 import urllib.request
 from sys import argv
