d40f709e03738080dbfdb7374b34b22bc9dd87d7
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 18:55:52 -0500

    Sends a POST request to the passed URL with the email as a parameter with package requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Sends a POST request to the passed URL with the email as a parameter,
+        and finally displays the body of the response. """
+from requests import post
+from sys import argv
+
+if __name__ == '__main__':
+    response = post(argv[1], {'email': argv[2]})
+    print(response.text)
