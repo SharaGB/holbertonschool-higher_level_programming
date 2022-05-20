b0da17e948c58892a31fd50737022839b416aaca
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 20:41:50 -0500

    List 10 commits (from the most recent to oldest) of the repository “rails”

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" List 10 commits (from the most recent to oldest)
+        of the repository “rails” """
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+    request = get('https://api.github.com/repos/{}/{}/commits'
+                  .format(argv[2], argv[1]))
+    commits = request.json()
+    for repo in commits[:10]:
+        print(repo.get('sha'), repo.get('commit').get('author').get('name'))
