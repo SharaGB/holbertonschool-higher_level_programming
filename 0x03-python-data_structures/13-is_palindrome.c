1b6e354c4ba139c45df21b23b976343565b6aa03
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-09 21:38:44 -0500

    Checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,32 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * is_palindrome - Functionthat checks if a singly linked list is a palindrome.
- * @head: Check head
+ * recursion - Function that use a recursion to check palindromic
+ * @head: Head of linked list
+ * @tail: Node that will be the tail of a singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int recursion(listint_t **head, listint_t *tail)
+{
+	int result = 0;
+
+	if (tail == NULL)
+		return (1);
+
+	result = recursion(head, tail->next);
+	if (result == 0)
+		return (0);
+	result = ((*head)->n == tail->n);
+	*head = (*head)->next;
+
+	return (result);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Head of the list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *low = *head;
-	listint_t *fast = *head;
-	int i = 0;
-
-	if (*head == NULL)
-	{
-		return (1);
-	}
-	if (*head != NULL && (*head)->next != NULL)
-	{
-		while (fast != NULL && fast->next != NULL)
-		{
-			while (i == 0)
-			{
-				fast = fast->next->next;
-				low = low->next;
-				i++;
-			}
-			return (1);
-		}
-	}
-	return (1);
+	if (!head)
+		return (0);
+	return (recursion(head, *head));
 }
