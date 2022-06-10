9721729bad3a7fc4f8953e7d2324b9458dfef144
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-09 21:53:11 -0500

    Python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -10,6 +10,6 @@ def magic_calculation(a, b):
                 raise Exception
             else:
                 result += (a ** b) / i
-        except:
+        except Exception as e:
             result = b + a
     return result
