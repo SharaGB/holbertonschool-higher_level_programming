2166e126348aaa2da3578d2279ef3785a654b6b7
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-09 22:08:08 -0500

    Add Python bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,17 +5,18 @@ import math
 
 class MagicClass:
     """ Magic class """
+
     def __init__(self, radius=0):
-        """ Initializes data """
+        """ Inizialize data """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('Radius radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """ Area of the magic class """
+        """ Area of the object """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """ Circumference of the magic class """
+        """ Circumference of the object """
         return 2 * math.pi * self.__radius
